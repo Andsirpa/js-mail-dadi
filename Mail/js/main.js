01b47fa6a1575dd44cabddfa7a099e33f6a168b2
Andsirpa <andsirpa94@gmail.com>
2024-01-18 15:33:19 +0100

    add validation condition

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,5 +1,5 @@
 // chiedo la mail all'utente
-const userMail = prompt('Iserisci la tua mail per accedere')
+let userMail = prompt('Iserisci la tua mail per accedere')
 console.log('Mail iserita: ' + userMail);
 
 // creo un array con le mail valide
@@ -9,3 +9,20 @@ const mails = [
     'mail3@gmail.com',
     'mail4@gmail.com'
 ]
+
+// creo un ciclo FOR per controllare la condizione
+
+let validMail = false
+for (let i = 0; i < mails.length; i++) {
+    if (userMail === mails[i]) {
+        validMail = true
+    }
+
+}
+
+// definisco la condizione
+if (validMail === true) {
+    console.log('Accesso consentito');
+} else {
+    console.log('Accessso negato');
+}
