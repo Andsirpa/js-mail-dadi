8fce890ca871c288e3070613e0e16a58602e4a13
Andsirpa <andsirpa94@gmail.com>
2024-01-18 14:33:00 +0100

    add mails

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,1 +1,11 @@
-console.log('JS OK');
+// chiedo la mail all'utente
+const userMail = prompt('Iserisci la tua mail per accedere')
+console.log('Mail iserita: ' + userMail);
+
+// creo un array con le mail valide
+const mails = [
+    'mail1@gmail.com',
+    'mail2@gmail.com',
+    'mail3@gmail.com',
+    'mail4@gmail.com'
+]
