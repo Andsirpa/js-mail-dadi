74d64ef8be25a72362da8d1440ffa71c71fae1fd
Andsirpa <andsirpa94@gmail.com>
2024-01-18 14:12:42 +0100

    add random numbers generator

--- Gioco dadi/js/main.js
+++ Gioco dadi/js/main.js
@@ -1,1 +1,9 @@
-console.log('JS OK');
+// genero un numero random per l'utente
+const userNumberGenerator = Math.floor(Math.random() * 6) + 1
+const userNumber = userNumberGenerator
+console.log('User number: ' + userNumber);
+
+// genero un numero random per il computer
+const computerNumberGenerator = Math.floor(Math.random() * 6) + 1
+const computerNumber = computerNumberGenerator
+console.log('Computer number: ' + computerNumber);
